1e17010d9ed445229d17e9d103c69e31fa4f64f0
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-23 20:32:46 -0300

    feat: Corrigindo responsividade

--- src/componentes/SideBar/index.tsx
+++ src/componentes/SideBar/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { UserPlus, SignOut, Database, UserCircle } from "phosphor-react"
 
 import { Link } from "react-router-dom"
@@ -8,7 +9,11 @@ import Logo from "../../assets/logo.jpg"
 
 
 export function SideBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
+    <>
+        {/* aside para telas maiores desktop*/}
         <aside className="hidden group sm:flex flex-col justify-evenly px-2 fixed top-0 left-0 h-full w-24 shadow-2xl bg-blue-900 transition-all duration-300 ease-in-out text-white hover:w-64 ">
             <div className="flex flex-col items-center">
                 <img 
@@ -57,5 +62,48 @@ export function SideBar() {
                 </div>
             </div>
         </aside>
+
+
+        {/* Manu Button para celular*/}
+        <div className="sm:hidden">
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="fixed top-4 left-4 z-50 p-2 bg-blue-900 text-white rounded-md">
+                    <div className="flex flex-col gap-1">
+                        <span className="block w-6 h-1 bg-white"></span>
+                        <span className="block w-6 h-1 bg-white"></span>
+                        <span className="block w-6 h-1 bg-white"></span>
+                    </div>
+                </button>
+
+                {/* Menu ativado no click */}
+                {isOpen && (
+                    <aside className="fixed top-0 left-0 h-full w-64 bg-blue-900 text-white shadow-lg z-40">
+                        <div className="flex justify-center items-center p-4">
+                            <img src={Logo} alt="Frontend Fusion logo" className="rounded-lg w-40" />
+                        </div>
+                        <div className="text-center mt-8">
+                            <Link
+                                to="/cadastro"
+                                className="block py-2 px-4 hover:bg-blue-800 hover:text-orange-400"
+                            >
+                                <UserPlus size={24} className="inline-block mr-2" /> Cadastro
+                            </Link>
+                            <Link
+                                to="/membros"
+                                className="block py-2 px-4 hover:bg-blue-800 hover:text-orange-400"
+                            >
+                                <Database size={24} className="inline-block mr-2" /> Membros
+                            </Link>
+                            <button onClick={() => {alert("Saindo..."); setIsOpen(false);}}
+                            className=" block py-2 px-4 hover:bg-blue-800 hover:text-orange-400 w-full">
+                                <SignOut size={24} className="inline-block mr-2" /> 
+                                Sair
+                            </button>
+                        </div>
+                    </aside>
+                )}
+            </div>        
+    </>    
     )
 }
